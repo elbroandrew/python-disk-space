73453bce9de2cbaae0bd5070c88c6549819e95bb
Andrew <elbroandrew@gmail.com>
2022-01-23 20:23:42 +1000

    feat: draw table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from shutil import disk_usage
+from texttable import Texttable
 
 """
 script for getting disk stats.
@@ -15,9 +16,22 @@ def get_disk_stats(path: str) -> dict:
     return dict(total=total, used=used, free=free)
 
 
+def draw_stats_table(dic: dict):
+    table = Texttable()
+    table.set_cols_align(["c", "c", "c"])
+    table.set_cols_valign(["m", "m", "m"])
+    table.set_precision(1)
+    table.add_rows([["Total", "Used", "Free"],
+                    [dic["total"],
+                     dic["used"],
+                     dic["free"]]
+                    ])
+    print(table.draw())
+
+
 def main():
     disk_info = get_disk_stats("C:")
-    print(disk_info)
+    draw_stats_table(disk_info)
 
 
 if __name__ == '__main__':
